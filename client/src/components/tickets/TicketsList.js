5dce1964b3923001dccc9fe3035fce9e27b31030
smamirshahi <smamirshahi@gmail.com>
2018-08-24 01:09:56 +0200

    worked on ticketlist styling

--- client/src/components/tickets/TicketsList.js
+++ client/src/components/tickets/TicketsList.js
@@ -1,16 +1,72 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types';
 import { getTickets, createTicket } from '../../actions/tickets'
 import { getEvents } from '../../actions/events'
 import { getUsers } from '../../actions/users'
 import CreateTicket from './CreateTicket'
 import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles';
+// import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+// import Collapse from '@material-ui/core/Collapse';
+// import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
 import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
-import Typography from 'material-ui/Typography'
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 import './TicketsList.css'
+import compose from 'recompose/compose';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 400,
+        margin: 50,
+        borderStyle: 'dashed',
+    },
+    risky: {
+        color: 'red'
+    },
+    normalRisk: {
+        color: 'orange'
+    },
+    safe: {
+        color: 'green'
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+        display: 'flex',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+        marginLeft: 'auto',
+        [theme.breakpoints.up('sm')]: {
+            marginRight: -8,
+        },
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+});
+
 
 class TicketsList extends PureComponent {
+    state = { expanded: false };
     componentWillMount() {
         if (this.props.event === null) this.props.getEvents()
         if (this.props.authenticated) {
@@ -22,23 +78,28 @@ class TicketsList extends PureComponent {
         this.props.createTicket(data.picture, data.price, data.description, this.props.match.params.id1)
     }
 
-    renderTicket = (ticket) => {
+    renderTicket = (ticket, classes) => {
         const { /* users,  */history } = this.props
         let riskClass
         if (ticket.risk > 70) riskClass = "risky"
         else if (ticket.risk > 30) riskClass = "normalRisk"
         else riskClass = "safe"
 
-
-        return (<Card key={ticket.id} className="ticket-card">
+        return (<Card key={ticket.id} className={`${classes.card}`}>
+            <CardHeader
+                title={`Seller: ${ticket.author}`}
+                subheader={`Price: € ${ticket.price}`}
+            />
             <CardContent>
-                <Typography variant="subheading" component="h2">
-                    <p className={riskClass}>Created by: {ticket.author}</p>
-                    <p>Risk: {ticket.risk}</p>
+                {/* <Typography variant="headline" component="h2">
+                    {event.title}
+                </Typography> */}
+                <Typography variant="subheading" component="h2" className={classes[riskClass]}>
+                    Risk: {ticket.risk} %
                 </Typography>
                 <CardMedia
-                    className="ticket-media"
-                    title={ticket.name}
+                    className={classes.media}
+                    title={ticket.author}
                     style={{ height: 100 }}
                     image={ticket.picture}
                 />
@@ -46,10 +107,14 @@ class TicketsList extends PureComponent {
                     {ticket.description}
                 </Typography>
             </CardContent>
-            <CardContent className="textss">
-                <p>{ticket.price}</p>
-                <p>{ticket.risk}</p>
-            </CardContent>
+            <CardActions className={classes.actions} disableActionSpacing>
+                <IconButton aria-label="Edit">
+                    <EditIcon />
+                </IconButton>
+                <IconButton aria-label="Label">
+                    <DeleteIcon />
+                </IconButton>
+            </CardActions>
             <CardActions>
                 <Button
                     size="small"
@@ -62,7 +127,7 @@ class TicketsList extends PureComponent {
     }
 
     render() {
-        const { event, /* tickets,  */users, /* authenticated *//* , createEvent */ } = this.props
+        const { event, /* tickets,  */users, classes /* authenticated *//* , createEvent */ } = this.props
         // console.log("tickets",this.props.tickets)
         // if (!authenticated) return (
         //     <Redirect to="/login" />
@@ -73,7 +138,7 @@ class TicketsList extends PureComponent {
         return (<Paper className="outer-paper">
             <h1>Event: {event.title}</h1>
             <div>
-                {Object.values(event.tickets).sort((a, b) => b.id - a.id).map(ticket => this.renderTicket(ticket))}
+                {Object.values(event.tickets).sort((a, b) => b.id - a.id).map(ticket => this.renderTicket(ticket, classes))}
                 {/* {event.tickets.map(ticket => this.renderTicket(ticket))} */}
             </div>
 
@@ -99,4 +164,13 @@ const mapDispatchToProps = {
     getEvents, getUsers, getTickets, createTicket
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TicketsList)
+TicketsList.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+// export default connect(mapStateToProps, mapDispatchToProps)(TicketsList)
+
+export default compose(
+    withStyles(styles),
+    connect(mapStateToProps, mapDispatchToProps)
+)(TicketsList);
